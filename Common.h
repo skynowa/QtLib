2c2a6c7eb640d6179845acc1d850bfae1b58b59f
skynowa <skynowa@gmail.com>
2012-06-12 11:35:59 +0200

    ADD: Common.h

---
+++ Common.h
@@ -0,0 +1,74 @@
+/**
+ * \file  Common.h
+ * \brief common header
+ */
+
+
+#ifndef QT_COMMON_H
+#define QT_COMMON_H
+//---------------------------------------------------------------------------
+// Core
+#include <Qt>
+#include <QtGlobal>
+#include <QTextCodec>
+#include <QDebug>
+#include <QClipboard>
+#include <QSignalMapper>
+#include <QTimer.h>
+
+// GUI
+#include <QtGui>
+#include <QCoreApplication>
+#include <QApplication>
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QMainWindow>
+#include <QDesktopWidget>
+#include <QWidget>
+#include <QShortcut>
+#include <QFileDialog>
+
+// Containers
+#include <QVector>
+#include <QList>
+#include <QStringList>
+#include <QSettings>
+#include <QFile>
+
+// Sql
+#include <QtSql>
+#include <QSqlTableModel>
+
+// Table
+#include <QHeaderView>
+#include <QItemSelectionModel>
+#include <QStandardItemModel>
+#include <QTableView>
+
+// Network
+#include <QUrl>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QNetworkAccessManager>
+
+// Other
+#include <QImage>
+
+// xLib
+#include <xLib/Common/xCommon.h>
+
+
+//---------------------------------------------------------------------------
+#define qNOT_IMPL               { QMessageBox::warning(this, qApp->applicationName(), tr(__FUNCTION__) + tr(": Not implemented")); }
+#define qMSG(s)                 { QMessageBox::information(NULL, qApp->applicationName(), s); }
+#define qCHECK_REF(var, object) { if (false == var) { QMessageBox::critical(0, qApp->applicationName(), object.lastError().text(),  QMessageBox::Cancel); } }
+#define qCHECK_PTR(var, object) { if (false == var) { QMessageBox::critical(0, qApp->applicationName(), object->lastError().text(), QMessageBox::Cancel); } }
+
+//---------------------------------------------------------------------------
+//converters
+#define qS2QS(s)                ( QString((s).c_str()) )
+    ///< convert std::tstring_t to QString
+#define qQ2S(qs)                ( std::tstring_t( (qs).begin(), (qs).begin() + (qs).size() ) )
+    ///< convert QString to std::tstring_t
+//---------------------------------------------------------------------------
+#endif // QT_COMMON_H
