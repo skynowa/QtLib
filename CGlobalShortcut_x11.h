0a81db81d32d3768252abe2cc3c2e640fd3c2efe
skynowa <skynowa@gmail.com>
2014-03-18 18:55:16 +0200

    CGlobalShortcut_x11: add

---
+++ CGlobalShortcut_x11.h
@@ -0,0 +1,21 @@
+/**
+ * \file   CGlobalShortcut_x11.h
+ * \brief  Global shortcut
+ */
+
+
+#ifndef QtLib_CGlobalShortcut_x11H
+#define QtLib_CGlobalShortcut_x11H
+//-------------------------------------------------------------------------------------------------
+#include "Common.h"
+//-------------------------------------------------------------------------------------------------
+class CGlobalShortcut_x11
+{
+public:
+             CGlobalShortcut_x11();
+    virtual ~CGlobalShortcut_x11();
+
+private:
+};
+//-------------------------------------------------------------------------------------------------
+#endif // QtLib_CGlobalShortcut_x11H
