c26755bce5399571d062868d659b7e5b9f5cc04e
skynowa <skynowa@gmail.com>
2014-03-19 11:03:00 +0200

    CGlobalShortcut_x11: update

--- CGlobalShortcut_x11.cpp
+++ CGlobalShortcut_x11.cpp
@@ -8,6 +8,20 @@
 
 #if defined(Q_OS_WIN)
     #include <windows.h>
+#elif defined(Q_OS_UNIX)
+    #include <QX11Info>
+
+    #include <X11/X.h>
+    #include <X11/Xlib.h>
+    #include <X11/keysym.h>
+
+    #if defined(KeyPress)
+        const int XKeyPress   = KeyPress;
+        const int XKeyRelease = KeyRelease;
+
+        #undef KeyPress
+        #undef KeyRelease
+    #endif
 #endif
 
 
@@ -17,14 +31,45 @@
 **************************************************************************************************/
 
 //-------------------------------------------------------------------------------------------------
-CGlobalShortcut_x11::CGlobalShortcut_x11()
+CGlobalShortcut_x11::CGlobalShortcut_x11(
+    QWidget *a_parent
+) :
+    QMainWindow(a_parent)
 {
+    qApp->installEventFilter(this);
+
+    m_keyCode = ::XKeysymToKeycode(QX11Info::display(), XK_F11);
+    ::XGrabKey(QX11Info::display(), m_keyCode, ControlMask | ShiftMask, QX11Info::appRootWindow(),
+        False, GrabModeAsync, GrabModeAsync);
+    ::XFlush(QX11Info::display());
 }
 //-------------------------------------------------------------------------------------------------
 /* virtual */
 CGlobalShortcut_x11::~CGlobalShortcut_x11()
 {
+    ::XUngrabKey(QX11Info::display(), m_keyCode, ControlMask|ShiftMask, QX11Info::appRootWindow());
+}
+//-------------------------------------------------------------------------------------------------
+bool
+CGlobalShortcut_x11::eventFilter(
+    QObject *a_object,
+    QEvent  *a_event
+)
+{
+    Q_UNUSED(a_object);
 
+    if (event->type() == QEvent::KeyPress) {
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+
+        if (keyEvent->modifiers() == (Qt::ControlModifier | Qt::ShiftModifier) &&
+            keyEvent->key() == Qt::Key_F11)
+        {
+            qDebug() << "Ctrl+Shift+F11 pressed!\n";
+            return false;
+        }
+    }
+
+    return true;
 }
 //-------------------------------------------------------------------------------------------------
 
--- CGlobalShortcut_x11.h
+++ CGlobalShortcut_x11.h
@@ -10,12 +10,16 @@
 #include "Common.h"
 //-------------------------------------------------------------------------------------------------
 class CGlobalShortcut_x11
+    : QMainWindow
 {
 public:
-             CGlobalShortcut_x11();
-    virtual ~CGlobalShortcut_x11();
+                 CGlobalShortcut_x11(QWidget *parent);
+    virtual     ~CGlobalShortcut_x11();
+
+    virtual bool eventFilter(QObject *object, QEvent *event);
 
 private:
+    int          _keyCode;
 };
 //-------------------------------------------------------------------------------------------------
 #endif // QtLib_CGlobalShortcut_x11H
