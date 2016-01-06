27c3e7f9ad30e4701565abf6d9de8732b1b6616e
skynowa <skynowa@gmail.com>
2016-01-06 16:12:10 +0200

    GlobalShortcut: review

--- GlobalShortcut/GlobalShortcut_impl.h
+++ GlobalShortcut/GlobalShortcut_impl.h
@@ -20,39 +20,38 @@ namespace qtlib
 class GlobalShortcut_impl :
     public Private<GlobalShortcut>
 #if !defined(Q_OS_MAC)
-    ,
-    public QAbstractNativeEventFilter
+    , public QAbstractNativeEventFilter
 #endif
 {
 public:
     QTLIB_DECLARE_PUBLIC(GlobalShortcut)
 
-    GlobalShortcut_impl();
-    ~GlobalShortcut_impl();
+                   GlobalShortcut_impl();
+    virtual       ~GlobalShortcut_impl();
 
-    bool enabled;
-    Qt::Key key;
+    bool           enabled;
+    Qt::Key        key;
     Qt::KeyboardModifiers mods;
 
-    bool setShortcut(const QKeySequence& shortcut);
-    bool unsetShortcut();
+    bool           setShortcut(const QKeySequence& shortcut);
+    bool           unsetShortcut();
 
-    static bool error;
+    static bool    error;
 
 #ifndef Q_OS_MAC
-    static int ref;
+    static int     ref;
 
-    virtual bool nativeEventFilter(const QByteArray & eventType, void *message, long *result);
+    virtual bool   nativeEventFilter(const QByteArray & eventType, void *message, long *result);
 #endif
 
-    static void activateShortcut(quint32 nativeKey, quint32 nativeMods);
+    static void    activateShortcut(quint32 nativeKey, quint32 nativeMods);
 
 private:
     static quint32 nativeKeycode(Qt::Key keycode);
     static quint32 nativeModifiers(Qt::KeyboardModifiers modifiers);
 
-    static bool registerShortcut(quint32 nativeKey, quint32 nativeMods);
-    static bool unregisterShortcut(quint32 nativeKey, quint32 nativeMods);
+    static bool    registerShortcut(quint32 nativeKey, quint32 nativeMods);
+    static bool    unregisterShortcut(quint32 nativeKey, quint32 nativeMods);
 
     static QHash<QPair<quint32, quint32>, GlobalShortcut*> shortcuts;
 };
