70715716314a6172fe5cefe03184fdbd377b58d3
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-03-04 18:09:29 +0200

    comment(s)

--- GlobalShortcut/GlobalShortcut_x11.cpp
+++ GlobalShortcut/GlobalShortcut_x11.cpp
@@ -24,6 +24,20 @@ namespace qtlib
 namespace
 {
 
+/**
+ * maskModifiers
+ *
+ * Mask        | Value | Key
+ * ------------+-------+------------
+ * ShiftMask   |     1 | Shift
+ * LockMask    |     2 | Caps Lock
+ * ControlMask |     4 | Ctrl
+ * Mod1Mask    |     8 | Alt
+ * Mod2Mask    |    16 | Num Lock
+ * Mod3Mask    |    32 | Scroll Lock
+ * Mod4Mask    |    64 | Windows
+ * Mod5Mask    |   128 | ???
+ */
 const QVector<quint32> maskModifiers =
     QVector<quint32>() << 0 << Mod2Mask << LockMask << (Mod2Mask | LockMask);
 //-------------------------------------------------------------------------------------------------
