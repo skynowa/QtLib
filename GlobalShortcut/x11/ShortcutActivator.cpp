98dcba8a159190d47c37c5914e79122cc2a9c4ee
skynowa <skynowa@gmail.com>
2021-02-25 04:07:58 +0200

    ShortcutActivator.cpp - review

--- GlobalShortcut/x11/ShortcutActivator.cpp
+++ GlobalShortcut/x11/ShortcutActivator.cpp
@@ -20,10 +20,10 @@ ShortcutActivator::run() /* override */
 {
     auto *display = static_cast<Display *>(this->display);
 
-    const Window       grab_window   = DefaultRootWindow(display);
-    const Bool         owner_events  = False;
-    const int          pointer_mode  = GrabModeAsync;
-    const int          keyboard_mode = GrabModeAsync;
+    const Window grab_window   { DefaultRootWindow(display) };
+    const Bool   owner_events  {False};
+    const int    pointer_mode  {GrabModeAsync};
+    const int    keyboard_mode {GrabModeAsync};
 
     ::XGrabKey(display, keycode, modifiers, grab_window, owner_events, pointer_mode, keyboard_mode);
     ::XGrabKey(display, keycode, modifiers | Mod2Mask, grab_window, owner_events, pointer_mode, keyboard_mode);
